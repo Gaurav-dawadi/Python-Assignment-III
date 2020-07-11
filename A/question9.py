104f3373e429e741d3742b0ba3304c9dba672bfb
Gaurav-dawadi <gaurabdawadi75@gmail.com>
2020-07-11 15:53:32 +0545

    Added question 8 and 9

--- A/question9.py
+++ A/question9.py
@@ -1,1 +1,26 @@
-# 
+# Tower of Hanoi
+
+count = 0
+A = [1, 2, 3, 4, 5]
+B = []
+C = []
+
+
+def tower_of_hanoi(A, B, C, n):
+    global count
+
+    if n == 1:
+        C.append(A.pop())
+        count += 1
+    else:
+        tower_of_hanoi(A, C, B, n-1)
+        tower_of_hanoi(A, B, C, 1)
+        tower_of_hanoi(B, A, C, n-1)
+    return A, B, C, count
+
+
+tower_of_hanoi(A, B, C, 5)
+print(A)
+print(B)
+print(C)
+print(count)
